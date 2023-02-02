1fd26cef55fcf5dc6a2098bee19bbf2e0bfeb062
Ostrovskyi <i.ostrovskyi@gmail.com>
2023-02-02 13:21:54 +0200

    refactoring

--- data/src/main/java/com/eleks/data/firebase/source/FirebaseDataSource.kt
+++ data/src/main/java/com/eleks/data/firebase/source/FirebaseDataSource.kt
@@ -4,14 +4,21 @@ import com.eleks.data.model.GroupDataModel
 import com.eleks.data.model.QuoteDataModel
 import com.eleks.data.model.ResultDataModel
 import com.eleks.data.model.SelectedGroupDataModel
+import kotlinx.coroutines.flow.MutableStateFlow
 
 interface FirebaseDataSource {
 
-    fun getGroups()
+     val groupsFlow: MutableStateFlow<ResultDataModel<List<GroupDataModel?>>>
 
-    fun getUserGroups()
+     val userGroupsFlow: MutableStateFlow<ResultDataModel<List<GroupDataModel?>>>
 
-    fun getUserSelectionGroups()
+     val selectedGroupsFlow: MutableStateFlow<ResultDataModel<List<SelectedGroupDataModel?>>>
+
+    fun subscribeGroups()
+
+    fun subscribeUserGroups()
+
+    fun subscribeSelectedGroups()
 
     suspend fun saveNewGroup(group: GroupDataModel): ResultDataModel<GroupDataModel>
 
